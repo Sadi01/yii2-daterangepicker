953b55ad5b36834c67bcf15cc198a311a5731b1a
Sadegh Shafii <sadshafiei.01@gmail.com>
2022-03-24 17:00:50 +0430

    Add FontAwesome

--- src/FontAwesomeAsset.php
+++ src/FontAwesomeAsset.php
@@ -8,7 +8,7 @@ use yii\web\AssetBundle;
 /**
  * @author Sadegh Shafii <sadshafiei.01@gmail.com>
  */
-class FontAwsomeAsset extends AssetBundle
+class FontAwesomeAsset extends AssetBundle
 {
     // The files are not web directory accessible, therefore we need
     // to specify the sourcePath property. Notice the @vendor alias used.
